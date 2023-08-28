9d8a094baaa12d3dc9129a18706c2e89e523578e
ajas <ajasshaffi123@gmail.com>
2023-08-28 18:59:21 +0530

    myfirstcommit

---
+++ strings.py
@@ -0,0 +1,16 @@
+name='Ajas'
+print(name.upper())
+print(name.islower())
+print(len(name))
+print(name.index('a'))
+print(name.replace('a', 'H'))
+print(min(1,3,5,90))
+print(round(3.2))
+print(bin(345))
+from math import*
+print(int(sqrt(100)))
+sentance=str(input("Enter the sentance:"))
+word1=input("The word to be replaced:")
+word2=input("The word to be replaced with")
+
+print(sentance.replace(word1,word2))
